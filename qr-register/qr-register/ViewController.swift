c2e7bb1dd654aee3cc60e81ea2af13f9837bf070
saranpol <saran@hlpth.com>
2016-03-23 16:00:00 +0700

    v0.2

--- qr-register/qr-register/ViewController.swift
+++ qr-register/qr-register/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController, UIGestureRecognizerDelegate {
 
+    @IBOutlet weak var mH_Logo: NSLayoutConstraint!
+    @IBOutlet weak var mW_Logo: NSLayoutConstraint!
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -17,6 +21,12 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate {
         
         // Enable swipe back when no navigation bar
         navigationController?.interactivePopGestureRecognizer!.delegate = self
+        
+        
+        if(UIScreen.mainScreen().bounds.width >= 768){
+            mW_Logo.constant = 157*1.5;
+            mH_Logo.constant = 143*1.5;
+        }
         
     }
     
@@ -31,6 +41,10 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate {
     @IBAction func clickBackEnd(_: AnyObject) {
         let url = NSURL(string: "http://www.singhayaijadhai.com/backend")!
         UIApplication.sharedApplication().openURL(url)
+    }
+    
+    override func prefersStatusBarHidden() -> Bool {
+        return true
     }
 }
 
--- qr-register/qr-register/ViewScan.swift
+++ qr-register/qr-register/ViewScan.swift
@@ -9,13 +9,43 @@
 import UIKit
 import AVFoundation
 
-class ViewScan: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
+class ViewScan: UIViewController, AVCaptureMetadataOutputObjectsDelegate, UIAlertViewDelegate {
     var captureSession: AVCaptureSession!
     var previewLayer: AVCaptureVideoPreviewLayer!
     @IBOutlet weak var mViewPreview: UIView!
+    @IBOutlet weak var mH_ViewPreview: NSLayoutConstraint!
+    @IBOutlet weak var mW_ViewPreview: NSLayoutConstraint!
+
+    
+    
+    @IBOutlet weak var mH_Logo: NSLayoutConstraint!
+    @IBOutlet weak var mW_Logo: NSLayoutConstraint!
+
+    @IBOutlet weak var mB_panel: NSLayoutConstraint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        mImageRegisted.hidden = true
+        mButtonHome.hidden = true
+        mButtonBack.hidden = false
+        mButtonRegister.hidden = true
+        mImagePleaseRegister.hidden = true
+        mLoading.hidden = true
+        
+        if(UIScreen.mainScreen().bounds.width >= 768){
+            mW_Logo.constant = 157*1.5;
+            mH_Logo.constant = 143*1.5;
+            mB_panel.constant = 32;
+        }
+        
+        
+        let w = UIScreen.mainScreen().bounds.width * 0.7
+        let h = UIScreen.mainScreen().bounds.width * 0.8
+        mW_ViewPreview.constant = w
+        mH_ViewPreview.constant = h
+        
         
         captureSession = AVCaptureSession()
         
@@ -47,12 +77,12 @@ class ViewScan: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
             return
         }
         
-        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession);
-        previewLayer.frame = mViewPreview.layer.bounds;
-        previewLayer.videoGravity = AVLayerVideoGravityResizeAspectFill;
-        mViewPreview.layer.addSublayer(previewLayer);
+        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        previewLayer.frame = CGRectMake(0, 0, w, h)
+        previewLayer.videoGravity = AVLayerVideoGravityResizeAspectFill
+        mViewPreview.layer.addSublayer(previewLayer)
         
-        captureSession.startRunning();
+        captureSession.startRunning()
     }
     
     func failed() {
@@ -96,16 +126,13 @@ class ViewScan: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
     
     func foundCode(code: String) {
         print(code)
-
-        let v = API.a.getViewController("Main", id: "ViewResult") as! ViewResult
-        v.mUID = code
-        
-        navigationController?.pushViewController(v, animated: true)
+        mUID = code
+        updateData()
     }
     
-//    override func prefersStatusBarHidden() -> Bool {
-//        return true
-//    }
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
 //    
 //    override func supportedInterfaceOrientations() -> UIInterfaceOrientationMask {
 //        return .Portrait
@@ -114,5 +141,127 @@ class ViewScan: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
     @IBAction func clickBack(_: AnyObject) {
         navigationController?.popViewControllerAnimated(true)
     }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    var mUID: String = ""
+    var mIsRegister = false
+    @IBOutlet weak var mImageRegisted: UIImageView!
+    @IBOutlet weak var mImagePleaseRegister: UIImageView!
+    @IBOutlet weak var mButtonHome: UIButton!
+    @IBOutlet weak var mButtonRegister: UIButton!
+    @IBOutlet weak var mButtonBack: UIButton!
+    @IBOutlet weak var mLoading: UIActivityIndicatorView!
+    
+    var mJson: AnyObject?
+ 
+    
+    func updateData() {
+        //mUID = "10205999986226652"
+        mLoading.hidden = false
+        mIsRegister = false
+        API.a.api_member(mUID) { (r) -> Void in
+            self.mLoading.hidden = true
+            if(r.result.error == nil){
+                self.mJson = r.result.value
+                self.updateUI()
+            }else{
+                let v = UIAlertView(title: "รหัสผู้ใช้ไม่ถูกต้อง", message: "กรุณาลองใหม่อีกครั้ง", delegate: self, cancelButtonTitle: "ตกลง")
+                v.show()
+            }
+        }
+    }
+    
+    func updateUI() {
+        let activate = API.a.getActivate(mJson)
+//                let activate = "0"
+        if(activate == "0"){
+            mImageRegisted.hidden = true
+            mButtonHome.hidden = true
+            mButtonBack.hidden = false
+            mButtonRegister.hidden = false
+            mImagePleaseRegister.hidden = false
+        }else if(activate == "1"){
+            mImageRegisted.hidden = false
+            mButtonHome.hidden = false
+            mButtonBack.hidden = true
+            mButtonRegister.hidden = true
+            mImagePleaseRegister.hidden = true
+        }else {
+            
+        }
+    }
+    
+    
+    func backStep() {
+        navigationController?.popViewControllerAnimated(true)
+    }
+    
+    
+    @IBAction func clickHome(_: AnyObject) {
+        backHome()
+    }
+    
+    func backHome() {
+        navigationController?.popToRootViewControllerAnimated(true)
+    }
+    
+    
+    func errorActivate() {
+        let v = UIAlertView(title: "ลงทะเบียนไม่สำเร็จ", message: "กรุณาลองใหม่อีกครั้ง", delegate: nil, cancelButtonTitle: "ตกลง")
+        v.show()
+    }
+    
+    func alertView(alertView: UIAlertView, didDismissWithButtonIndex buttonIndex: Int) {
+        if(mIsRegister){
+
+        }else{
+            if (captureSession?.running == false) {
+                captureSession.startRunning();
+            }
+        }
+    }
+    
+    @IBAction func clickRegister(_: AnyObject) {
+        mImageRegisted.hidden = true
+        mButtonHome.hidden = true
+        mButtonBack.hidden = false
+        mButtonRegister.enabled = false
+        mLoading.hidden = false
+        mIsRegister = true
+        
+        API.a.api_activate(mUID) { (r) -> Void in
+            self.mLoading.hidden = true
+            self.mButtonRegister.enabled = true
+            
+            
+            if(r.result.error == nil){
+                let activate = API.a.getActivate(r.result.value)
+//                let activate = "ccc"
+                if(activate == "ok"){
+                    self.mImagePleaseRegister.hidden = true
+                    self.mImageRegisted.hidden = false
+                    self.mButtonHome.hidden = false
+                    self.mButtonBack.hidden = true
+                    self.mButtonRegister.hidden = true
+                }else{
+                    self.errorActivate()
+                }
+            }else{
+                self.errorActivate()
+            }
+        }
+    }
+    
+    
     
 }
